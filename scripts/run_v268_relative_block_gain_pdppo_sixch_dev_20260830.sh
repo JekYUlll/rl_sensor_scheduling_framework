211c9570986c2f3a33bb997f7996702cb1f88de5
JekYUlll <jekyulll@gmail.com>
2026-08-30 17:22:33 +0800

    Parameterize block reward credit mode

--- scripts/run_v268_relative_block_gain_pdppo_sixch_dev_20260830.sh
+++ scripts/run_v268_relative_block_gain_pdppo_sixch_dev_20260830.sh
@@ -11,6 +11,9 @@ else
 fi
 
 REWARD_PROXY_MODE=forecast_block_relative_gain \
+DECISION_ONLY_POLICY_UPDATES="${DECISION_ONLY_POLICY_UPDATES:-0}" \
+DECISION_BLOCK_CREDIT="${DECISION_BLOCK_CREDIT:-0}" \
+DECISION_BLOCK_REWARD_MODE="${DECISION_BLOCK_REWARD_MODE:-sum}" \
 RUN_PREFIX="${RUN_PREFIX:-v268_relative_block_gain_pdppo_sixch_dev}" \
 LOG_PREFIX="${LOG_PREFIX:-v268_relative_block_gain_pdppo_sixch}" \
 bash scripts/run_v267_block_gain_pdppo_sixch_dev_20260830.sh "${SEEDS[@]}"
